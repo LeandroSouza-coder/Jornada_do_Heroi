53e5a39a5e2394ed909f0a4755caa343bc3b118a
LeandroSouza-coder <83925147+LeandroSouza-coder@users.noreply.github.com>
2025-01-31 17:54:36 -0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+// Lê a posição inicial do herói
+const posicaoInicial = parseInt(gets());
+// Lê o número total de passos que o herói deve dar
+const totalPassos = parseInt(gets());
+
+// Calcula a posição final do herói
+const posicaoFinal = posicaoInicial + totalPassos;
+
+// Imprime a posição final
+print("Posicao final do heroi:", posicaoFinal);
